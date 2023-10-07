61e611458bd25b8ef0dd28c102eedc81b34e3119
Rasmus Jokinen <raajok@utu.fi>
2023-10-07 19:57:58 +0300

    JDA setup and manager for commands

---
+++ src/main/java/com/raajok/CommandManager.java
@@ -0,0 +1,49 @@
+package com.raajok;
+
+import com.raajok.commands.Command;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Manages setting up and executing all the slash commands.
+ */
+public class CommandManager extends ListenerAdapter {
+
+    private List<Command> commandList = new ArrayList<>();
+
+    public void addCommand(Command command) {
+        this.commandList.add(command);
+    }
+
+    /**
+     * Sets up all the commands added to CommandManager on bot startup
+     * @param event
+     */
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+        for (Guild guild : event.getJDA().getGuilds()) {
+            for (Command command : this.commandList) {
+                guild.upsertCommand(command.getName(), command.getDescription()).addOptions(command.getOptions());
+            }
+        }
+    }
+
+    /**
+     * Executes the correct command from the Command list when a slash command is used
+     * @param event
+     */
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        for (Command command : this.commandList) {
+            if (event.getName().equals(command.getName())) {
+                command.execute(event);
+            }
+        }
+    }
+}
--- src/main/java/com/raajok/DotaOracle.java
+++ src/main/java/com/raajok/DotaOracle.java
@@ -1,2 +1,25 @@
-package com.raajok;public class DotaOracle {
+package com.raajok;
+
+import com.raajok.commands.HelloWorldCommand;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class DotaOracle {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        final String TOKEN = dotenv.get("DISCORD_TOKEN");
+
+        JDA jda = JDABuilder.createDefault(TOKEN).build();
+
+        CommandManager cmdManager = new CommandManager();
+        addCommands(cmdManager);
+
+        jda.addEventListener(cmdManager);
+    }
+
+    private static void addCommands(CommandManager cmdManager) {
+        cmdManager.addCommand(new HelloWorldCommand());
+    }
 }
