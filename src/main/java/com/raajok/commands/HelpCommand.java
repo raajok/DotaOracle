dcc3af6ef4fd4ab2aec8bf2d839713a8927a8048
Rasmus Jokinen <raajok@utu.fi>
2024-06-12 21:41:30 +0300

    Update help command

--- src/main/java/com/raajok/commands/HelpCommand.java
+++ src/main/java/com/raajok/commands/HelpCommand.java
@@ -34,9 +34,11 @@ public class HelpCommand implements Command {
 
         Map<String, String> commandDescriptions = new HashMap<>();
         commandDescriptions.put("help", "Opens up this info message");
+        commandDescriptions.put("herostats [hero] [id]", "Shows hero-specific stats for the player owning the Steam ID given.");
+        commandDescriptions.put("leipägang", "Shows Leipägang's win/loss statistics and brief statistics for all members individually.");
+        commandDescriptions.put("records", "Shows the records of Leipägang. The records are taken from the Dotabuff's record page.");
         commandDescriptions.put("search [name]", "Searches players by name and gives top 5 results and their Steam IDs. Mostly used for copy-pasting SteamIDs for other commands.");
         commandDescriptions.put("wl [id] {amount}", "Shows wins, losses and win percent for the player owning the Steam ID given. Can be limited to {amount} of recent games.");
-        commandDescriptions.put("herostats [hero] [id]", "Shows hero-specific stats for the player owning the Steam ID given.");
 
         EmbedBuilder embed = new EmbedBuilder();
         embed.setTitle("Help");
